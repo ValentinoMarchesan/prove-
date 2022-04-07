4f5603ddf3dfa8ca7782d505cf3285af27025a5d
Valentino Marchesan <valentino.marchesan@studenti.unipd.it>
2022-04-07 19:12:57 +0200

    aggiunto stringa maria ecc...

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyHomePage extends StatefulWidget {
   int maria = 1;
   int convento = 2;
   int wtf = 7;
-  int asdfgh = 2;
+  String mariabestemmia = 'dio...';
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
