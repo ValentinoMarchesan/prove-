fd5e21f862d45b8c96511320576056c1ad8b49b5
Valentino Marchesan <valentino.marchesan@studenti.unipd.it>
2022-04-07 18:33:26 +0200

    ho aggiunto convento

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
   int banana = 0;
   int maria = 1;
-
+  int convento = 2;
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
